9a7616f0235d89677a4c0396cda0aa3e18f31319
Raza Mahmood <razammahmood@gmail.com>
2023-10-09 22:26:25 -0400

    comment out unused code for now

--- src/index.js
+++ src/index.js
@@ -11,12 +11,12 @@ import core from '@actions/core';
 import { glob } from 'glob';
 
 const SOURCE = core.getInput('source');
-const DESTINATION = core.getInput('destination');
+//const DESTINATION = core.getInput('destination');
 const IGNORE = core.getInput('ignore');
 const BUCKET_NAME = core.getInput('bucket');
-const ACCESS_KEY_ID = core.getInput('access_key');
-const SECRET_ACCESS_KEY = core.getInput('secret_key');
-const REGION = core.getInput('region');
+//const ACCESS_KEY_ID = core.getInput('access_key');
+//const SECRET_ACCESS_KEY = core.getInput('secret_key');
+//const REGION = core.getInput('region');
 
 async function getFiles(src, ignore) {
   const files = await glob(src, { ignore, signal: AbortSignal.timeout(10000), withFileTypes: true });
